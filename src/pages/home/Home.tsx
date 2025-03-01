72805e7a2d5a911e55a71b8acdb8a2cfc6dc06a5
Alessandro <abcsandro@hotmail.com>
2025-03-01 10:40:27 -0300

    Ajustes nos vídeos

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,7 +7,7 @@ import InfoBox from "../../components/common/InfoBox";
 
 import giftsData from "../../db/gift.json";
 
-import backgroundVideo from "../../Assets/video/VIDEO ABERTURA SITE .mp4"
+import backgroundVideo from "../../Assets/video/Video Abertura Site.mp4"
 import Gabriel from "../../Assets/img/Gabriel.jpg"
 import Thiago from "../../Assets/img/Thiago.jpg"
 import Lucas from "../../Assets/img/Lucas.jpg"
