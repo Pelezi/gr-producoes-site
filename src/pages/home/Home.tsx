a7333ac4525955c632915645e702e96cf8a32499
Alessandro <abcsandro@hotmail.com>
2025-03-06 20:45:44 -0300

    Rollback video da tela inicial

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,6 +5,7 @@ import styles from "./Home.module.css";
 import Title from "../../components/common/Title";
 
 
+import backgroundVideoDesktop from "../../Assets/video/Video Abertura Site.mp4"
 import backgroundVideoMobile from "../../Assets/video/Video Abertura Site_phone.mp4"
 
 import Gabriel from "../../Assets/img/Gabriel.jpg"
@@ -14,7 +15,15 @@ import ClientCarousel from "../../components/ClientCarousel/ClientCarousel";
 import WhatsappContact from "../../components/WhatsappContact/WhatsappContact";
 
 const Home = () => {
-    const backgroundVideo = backgroundVideoMobile;
+    const [backgroundVideo, setBackgroundVideo] = useState<string>(backgroundVideoDesktop);
+
+    useEffect(() => {
+        if (window.innerWidth < 768) {
+            setBackgroundVideo(backgroundVideoMobile);
+        } else {
+            setBackgroundVideo(backgroundVideoDesktop);
+        }
+    }, []);
 
     return (
         <div>
