c126be18c46f389102fe44df338b28e061b58891
Alessandro <abcsandro@hotmail.com>
2025-03-09 13:46:25 -0300

    Adjusted video styles

--- src/components/VideoGallery/VideoGallery.tsx
+++ src/components/VideoGallery/VideoGallery.tsx
@@ -12,15 +12,37 @@ const InfoBox: React.FC<VideoGalleryProps> = ({ video }) => {
     const videoRef = useRef<HTMLVideoElement>(null);
 
     useEffect(() => {
+        const handleCanPlayThrough = () => {
+            videoRef.current?.play();
+        };
+
+        const handlePlay = () => {
+            if (videoRef.current) {
+                videoRef.current.addEventListener('canplaythrough', handleCanPlayThrough);
+                videoRef.current.play();
+            }
+        };
+
         if (videoRef.current) {
             const videoWidth = videoRef.current.offsetWidth;
             divRef.current?.style.setProperty('--video-width', `${videoWidth}px`);
+            videoRef.current.addEventListener('play', handlePlay);
         }
+
+        return () => {
+            videoRef.current?.removeEventListener('canplaythrough', handleCanPlayThrough);
+            videoRef.current?.removeEventListener('play', handlePlay);
+        };
     }, []);
 
     return (
         <div ref={divRef} className={video.orientation === "portrait" ? styles.videoPortrait : styles.videoLandscape}>
-            <video ref={videoRef} controls controlsList="nodownload" >
+            <video
+                ref={videoRef}
+                controls
+                controlsList="nodownload"
+                preload="metadata"
+            >
                 <source src={video.url} type="video/mp4" />
             </video>
             <p className={styles.title}>{video.title}</p>
