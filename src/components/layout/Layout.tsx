8ca0acc1a560e9137cfe4bf130a1f046bd8cd613
Alessandro <abcsandro@hotmail.com>
2025-03-15 22:01:37 -0300

    Update cards animation

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import Menu from "./Menu";
 import Footer from "./Footer";
-import Loading from "../common/Loading/Loading";
 
 import styles from "./Layout.module.css";
-import { useNavigate } from "react-router-dom";
 
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
@@ -15,46 +13,10 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-    const [loading, setLoading] = useState(true);
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const handleLoad = () => {
-            setTimeout(() => {
-                setLoading(false);
-            }, 1000);
-        };
-
-        const handleDOMContentLoaded = () => {
-            setTimeout(() => {
-                setLoading(false);
-            }, 1000);
-        };
-
-        const handleTimeout = () => {
-            setLoading(false);
-        };
-
-        if (document.readyState === "loading") {
-            document.addEventListener("DOMContentLoaded", handleDOMContentLoaded);
-        } else {
-            handleDOMContentLoaded();
-        }
-
-        window.addEventListener("load", handleLoad);
-        const timeoutId = setTimeout(handleTimeout, 5000);
-
-        return () => {
-            window.removeEventListener("load", handleLoad);
-            document.removeEventListener("DOMContentLoaded", handleDOMContentLoaded);
-            clearTimeout(timeoutId);
-        };
-    }, []);
 
     return (
         <>
-            <Loading isLoading={loading} />
-            <div className={`${styles.container} ${loading ? styles.hidden : ""}`}>
+            <div className={`${styles.container}`}>
                 <div className={styles.main}>
                     <div className={styles.content}>
                         {children}
